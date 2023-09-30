6626a83dd2dc700c30e84bb6d102d74d574512b9
Julio Cesar Polmann <juliocesarpolmann@gmail.com>
2023-09-30 17:00:35 -0300

    add eventos seeders from json

--- campus360-api/database/seeders/Atividade.ts
+++ campus360-api/database/seeders/Atividade.ts
@@ -1,8 +1,24 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
-import { AtividadeFactory } from 'Database/factories'
+import Atividade from 'App/Models/Atividade'
+import fs from 'fs'
 
 export default class extends BaseSeeder {
   public async run () {
-    await AtividadeFactory.createMany(10)
+    const atividades = fs.readFileSync('eventos.json', 'utf-8')
+    const atividadesJson = JSON.parse(atividades)
+
+    for (let i = 0; i < atividadesJson.length; i++) {
+      const atividade = atividadesJson[i]
+      const atividadeObj = {
+        nome: atividade.acf.evento_nome,
+        descricao: atividade.acf.evento_descricao,
+        data: atividade.date,
+        local: atividade.acf.evento_local,
+        tipo: atividade.type,
+        livre: true,
+        imagemUrl: atividade.acf.evento_banner.url,
+      }
+      await Atividade.create(atividadeObj)
+    }
   }
 }
